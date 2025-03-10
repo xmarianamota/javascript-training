f0c6dfbaadf71eb901f6940007430b51aeb153e3
xmarianamota <xmarianamota@gmail.com>
2025-03-10 17:37:54 +0200

    2.24 done

---
+++ basics-02-added-scripts-imports/assets/scripts/app.js
@@ -0,0 +1,6 @@
+let currentResult = 0; 
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1'; 
+
+outputResult(currentResult, calculationDescription);
