e38c138932e5d7d50aad64244e752f0e872ac85a
xmarianamota <xmarianamota@gmail.com>
2025-03-15 15:41:07 +0200

    2.36 done &  assigment done

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,26 @@
 const task3Element = document.getElementById('task-3');
+
+
+function alertFunctionNoParameter() {
+    alert('Testing alertFunction, it works!');
+    return
+ } 
+
+ function alertFunctionWithParameter(parameter) {
+    alert(parameter);
+    return
+ }
+ 
+ function brandNewFunction(param1, param2, param3) {
+    const concatenatedText = `${param1} ${param2} ${param3}`;
+    return concatenatedText
+ }
+
+
+
+task3Element.addEventListener('click', alertFunctionNoParameter)
+let concatenatedString = brandNewFunction('parameter1', 'parameter2', 'parameter3');
+alert(concatenatedString);
+
+// or alternatively:
+// alert(brandNewFunction('parameter1', 'parameter2', 'parameter3'));
